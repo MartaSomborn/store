dca29b373a3e05264c715e4b1d6beec62053821a
martex <marta.somborn@gmail.com>
2023-05-04 14:34:38 +0200

    add Favourite

--- src/components/main/ProductItem.tsx
+++ src/components/main/ProductItem.tsx
@@ -1,14 +1,55 @@
-import { Typography } from "@mui/material";
+import { Button, Typography } from "@mui/material";
 import Box from "@mui/material/Box";
-import { TypeFormatFlags } from "typescript";
+import { useState } from "react";
+import IconButton from "@mui/material/IconButton/IconButton";
+
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 
 const ProductItem = (props: any) => {
-  console.log(props);
+  //console.log(props);
+
+  const [showAddToFavourite, setShowAddToFavourite] = useState(false);
+
+  const handleMouseOver = () => {
+    setShowAddToFavourite(true);
+  };
+
+  const handleMouseOut = () => {
+    setShowAddToFavourite(false);
+  };
   return (
     // Box is like div
-    <Box sx={{ width: "200px", height: "200px", border: "2px solid black" }}>
-      <Typography>{props.product.name}</Typography>
-      <Typography>{props.product.price}</Typography>
+    <Box
+      sx={{
+        width: "200px",
+        height: "200px",
+        border: "2px solid black",
+        display: "flex",
+        flexDirection: "row",
+        flexWrap: "wrap",
+        alignItems: "flex-start",
+        justifyContent: "space-between",
+      }}
+      onMouseOver={handleMouseOver}
+      onMouseOut={handleMouseOut}
+    >
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          flexWrap: "wrap",
+          alignItems: "flex-start",
+        }}
+      >
+        <Typography>{props.product.name}</Typography>
+        <Typography>{props.product.price}</Typography>
+        <Button>Add to box</Button>
+      </div>
+      {showAddToFavourite && (
+        <IconButton>
+          <FavoriteBorderIcon sx={{ width: "2em", height: "2em" }} />
+        </IconButton>
+      )}
     </Box>
   );
 };
